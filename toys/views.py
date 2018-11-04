dea161197d8f0167aa3af42bb4be3a5657301db2
viswam <vbogaraj@cisco.com>
2018-11-04 17:27:03 +0530

    Added views, routes to create REST endpoints

--- toys/views.py
+++ toys/views.py
@@ -1,3 +1,60 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from toys.models import Toy
+from toys.serializers import ToySerializer
+from django.views.decorators.csrf import csrf_exempt
+
+
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def toy_list(request):
+    if request.method == 'GET':
+        toys = Toy.objects.all()
+        toys_serializer = ToySerializer(toys, many=True)
+        return JSONResponse(toys_serializer.data)
+
+    if request.method == 'POST':
+        print("Request data: " + str(request))
+        toy_data = JSONParser().parse(request)
+        print(toy_data)
+        toy_serializer = ToySerializer(data=toy_data)
+        if toy_serializer.is_valid():
+            toy_serializer.save()
+            return JSONResponse(toy_serializer.data, status=status.HTTP_201_CREATED)
+        return JSONResponse(toy_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@csrf_exempt
+def toy_detail(request, pk):
+    try:
+        toy = Toy.objects.get(pk=pk)
+    except:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        toy_serializer = ToySerializer(toy)
+        return JSONResponse(toy_serializer.data)
+
+    elif request.method == 'PUT':
+        toy_data = JSONParser().parse(request)
+        toy_serializer = ToySerializer(toy, data=toy_data)
+        if toy_serializer.is_valid():
+            toy_serializer.save()
+            return JSONResponse(toy_serializer.data)
+        return JSONResponse(toy_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        toy.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
+
